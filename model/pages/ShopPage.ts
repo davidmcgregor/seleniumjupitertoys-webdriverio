5c69756db14da91d77e9d4879b2106edf9a1a4b9
Juan Florez <jflorez@planittesting.com>
2020-06-05 19:24:38 +1000

    code refactor

--- model/pages/ShopPage.ts
+++ model/pages/ShopPage.ts
@@ -5,7 +5,7 @@ export default class ShopPage extends BasePage {
     public getProduct(comparator: (p: Product) => boolean): Product {
         const product = this.getProducts(comparator)[0];
         if (product) return product;
-        throw `Product not found using: ${comparator}`;
+        else throw `Product not found using: ${comparator}`;
     }
 
     public getProducts(comparator: (p: Product) => boolean): Array<Product> {
