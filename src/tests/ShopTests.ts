854739bdc972a77c21806fed3532ed30030f8c34
Juan Florez <jflorez@planittesting.com>
2020-11-29 10:56:00 +0000

    refactor tests

--- src/tests/ShopTests.ts
+++ src/tests/ShopTests.ts
@@ -1,34 +1,30 @@
 import {suite, test} from '@testdeck/mocha';
 import {HomePage, ShopPage, open} from '../model/pages';
-import {expect} from 'chai';
-import {Product} from '../model/components';
+import 'chai/register-should';
 
 @suite
 export class ShopTests {
     @test
     'validate product price'(): void {
-        expect(
-            open(HomePage)
-                .clickShopMenu()
-                .getProduct(p => p.getTitle() === 'Fluffy Bunny')
-                .getPrice()
-        ).to.equal(8.99);
+        open(HomePage)
+            .clickShopMenu()
+            .getProduct(p => p.getTitle() === 'Fluffy Bunny')
+            .getPrice()
+            .should.equal(8.99);
     }
 
     @test
     'validate products above minumum'(): void {
-        expect(
-            open(HomePage).clickShopMenu()
-                .getProducts(p => p.getPrice() < 8.99)
-                .length
-        ).to.equal(0);
+        open(HomePage).clickShopMenu()
+            .getProducts(p => p.getPrice() < 8.99)
+            .length
+            .should.equal(0);
     }
 
     @test
     'buy product with number of stars'(): void {
         const shopPage: ShopPage = open(HomePage).clickShopMenu();
         shopPage.getProduct(p => p.getStars() === 5).clickBuyButton();
-
-        expect(shopPage.getCartCount()).to.equal(1);
+        shopPage.getCartCount().should.equal(1);
     }
 }
